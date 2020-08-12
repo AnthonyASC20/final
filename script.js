b1ece73f62885f076137e707d749fcf45cbf6fd4
AnthonyASC20 <anthonymlop@icloud.com>
2020-08-12 00:22:51 -0400

    update

--- script.js
+++ script.js
@@ -1,13 +1,75 @@
+const subjectElement = document.getElementById("testSubject");
+const topicElement = document.getElementById("testTopic");
+const button = document.getElementById("submitButton");
+button.addEventListener("click",updateDB);
+
+//Set database object here
+const database = firebase.database().ref();
+
+/**
+ * Updates the database with the username and message.
+ */
+function updateDB(event){
+    event.preventDefault(); //does not refresh page
+    const subject        = subjectElement.value;
+    const topic         = topicElement.value;
+
+    subjectElement.value = "";
+    topicElement.value  = "";
+
+    console.log(subject + "  " + topic);
+
+    //Update database here
+    // let value = {
+    //     subject: subject,
+    //     topic: topic
+    // }
+
+    // database.push(value);
+}
+
+// Set database "child_added" event listener here
+// database.on('child_added', addMessage);
+
+// let allMessages = document.querySelector('.allMessages');
+
+
+// function addMessage(data) {
+//     let value = data.val();
+//     let subject= value.subject;
+//     let topic = value.topic;
+
+//     let p = document.createElement('p');
+//     p.innerText = subject + " " + topic;
+//     allMessages.appendChild(p);
+// }
+
 let userInput = "cellular respiration";
 let khan = "khan academy";
 
-fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyBLeY71qZL_t-T6t8TFGPprKxuZEIEUxpY&type=video&q=' + userInput + khan)
+fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyC9uKfXBjYpoiQjVbLmNgFbN8AQaDujiks&type=video&q=' + userInput + khan)
         .then(response =>
         response.json()
-        )
+       ) 
     .then(json =>
         console.log(json)
         );
+
+        // let url ="https://randomuser.me/api";
+
+        // fetch(url)
+        //     .then(function(response){
+        //         console.log(response);
+        //         return response.json();
+        //     })
+        //     .then(function(myJson){
+        //         console.log(myJson);
+        //         let email= myJson[0].results.email;
+        //         const p = document.createElement("p");
+        //         p.innerText=email;
+        //         document.body.appendChild(p);
+        //     })
+
 
 
 
